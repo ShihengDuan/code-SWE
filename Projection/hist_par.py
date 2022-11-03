2b288966a397905b5492936ceb7832ab1e980ef1
ShihengDuan <shiduan@ucdavis.edu>
2022-11-03 10:05:50 -0700

    add args

--- Projection/hist_par.py
+++ Projection/hist_par.py
@@ -19,18 +19,49 @@ from models.lstm import LSTM
 from models.tcnn import TCNN
 from models.attention import Attention
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str)
-parser.add_argument('--ens', type=int, help="model ensemble member")
-args = vars(parser.parse_args())
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ens', type=int)
+    parser.add_argument('--device', type=int, default=-1)
+    parser.add_argument('--model', type=str, default='lstm')
+    parser.add_argument('--mountain', type=str, default='rocky')
+    ## Mountain: Rocky, Sierra, Northern, Western, Cascade
+    args = vars(parser.parse_args())
+    return args
+
+args = get_args()
 ens = args['ens']
 # model_type = 'TCNN'
 model_type = 'LSTM'
 # model_type = 'Attention'
 path = 'LSTM_proj/'
 
-device = torch.device('cuda:1')
+device_id = args['device']
+devices = [torch.device('cuda:0'), torch.device('cuda:1'), torch.device('cuda:2'), torch.device('cuda:3')]
+print(ens, ' STARTED')
+if device_id !=-1:
+    device = devices[device_id] # device to use from the 4 GPUs.
+else:
+    device = devices[ens%4]
+mountain = args['mountain']
+if mountain.upper()=='ROCKY':
+    num_lat = 128
+    num_lon = 85
+elif mountain.upper()=='SIERRA':
+    num_lat = 72
+    num_lon = 64
+elif mountain.upper()=='CASCADE':
+    num_lat = 133
+    num_lon = 52
+elif mountain.upper()=='NORTH':
+    num_lat = 90
+    num_lon = 141
+elif mountain.upper()=='UTAH':
+    num_lat = 78
+    num_lon = 86
+else:
+    print('Mountain Not Supported! Go back and write your code. ')
 HID = 128
 KER = 7
 LEVELS = 5
@@ -49,18 +80,20 @@ forcings = {'pr': model + '-hist-pr.nc', 'rave': model + '-hist-rave.nc',
             'tmmx': model + '-hist-tmmx.nc', 'vs': model + '-hist-vs.nc'}
 
 n_inputs = len(attributions) + len(forcings)
-topo_file = 'topo_file_LOCA.nc'
-
+# topo_file = 'topo_file_LOCA.nc'
 
 # topo_file = 'topo_file_LOCA_regrid.nc'
-
+topo_file = 
 
 def inference(lat_id, device=device):
     ds = CO_LOCADataset(forcings=forcings, lat=lat_id, attributions=attributions, topo_file=topo_file,
-                        window_size=180, scenario='hist')
+                        window_size=180, scenario='hist',
+                        scaler_mean='../gridMET/Rocky/gridmet_mean_norm.nc', # this is mean from SNOTEL stations. should be static. 
+                        scaler_std='../gridMET/Rocky/gridmet_std_norm.nc')
     if model_type == 'LSTM':
         model = LSTM(hidden_units=HID, input_size=n_inputs, relu_flag=RELU_FLAG)
-        model_path = '/tempest/duan0000/SWE/gridMET/runs_relative_proj/LSTM_1e-4/model_ens_' + str(ens)
+        # model_path = '/tempest/duan0000/SWE/gridMET/runs_relative_proj/LSTM_1e-4/model_ens_' + str(ens)
+        model_path = '../gridMET/runs/runs_30m_relative_median/LSTM/model_ens_' + str(ens)
     elif model_type == 'TCNN':
         model = TCNN(kernal_size=KER, num_levels=LEVELS, num_channels=CHA, input_size=n_inputs)
         model_path = '/tempest/duan0000/SWE/gridMET/runs_relative_clean/TCNN_1e-4/model_ens_' + str(ens)
@@ -91,14 +124,14 @@ if __name__ == '__main__':
     except RuntimeError:
         pass
 
-    prediction = np.empty((112, 72, 7126))
+    prediction = np.empty((num_lat, num_lon, 7126))
 
     with Pool(4) as pool:
-        lat_ids = np.arange(112)
+        lat_ids = np.arange(num_lat)
         for results in tqdm(pool.imap(inference, lat_ids), total=len(lat_ids), position=0):
             lat, pred = results
-            split_data = np.split(pred, 72)
+            split_data = np.split(pred, num_lon)
             # print(lat, ' ', len(pred), ' ', len(pred) / 108, ' ', split_data[0].shape)
-            for i in range(72):
+            for i in range(num_lon):
                 prediction[lat, i] = split_data[i]
     np.save(path + model + '-hist_' + str(ens), prediction)
