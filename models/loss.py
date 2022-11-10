0bb569992a67fd16787b6289c7cf8a70271456b7
Shiheng <shiduan@ucdavis.edu>
2022-11-09 19:51:59 -0800

    L12 loss

--- models/loss.py
+++ models/loss.py
@@ -1,5 +1,5 @@
 import torch
-
+from torch.nn import L1Loss
 
 class LossWithStd(torch.nn.Module):
     def __init__(self, eps=0.1):
@@ -27,3 +27,14 @@ class LossWithPenalty(torch.nn.Module):
         loss = torch.mean((y_hat - y) ** 2)
         penalty = torch.mean(1 / (y - ymin + 0.1) * (y_hat - y) ** 2)
         return loss + penalty
+
+class L12loss(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        l1loss = L1Loss()
+    def forward(self, y_hat, y, ymin=0):
+        ind = y>ymin
+        L1 = torch.sum(torch.abs((y_hat-y)*(1-ind)))
+        L2 = torch.sum(torch.square((y_hat-y)*ind))
+        loss = (L1+L2)/y_hat.nelement()
+        return loss
